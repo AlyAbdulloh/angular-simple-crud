65f9e5018c607e6ca4a52e30269a3c646361e91d
AlyAbdulloh <uwongsabar12@gmail.com>
2024-05-05 21:57:53 +0700

    learn pipe and funtion of interface in angular

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
@@ -7,34 +7,46 @@ import { AuthService } from '../services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
   password!: string;
   show: boolean = false;
   isValid: boolean = true;
+  array: any[] = [1, 2, 3, 4];
 
-  constructor(private http:HttpClient, private router:Router, private fb: FormBuilder, private authServices: AuthService){}
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private fb: FormBuilder,
+    private authServices: AuthService
+  ) {
+    console.log('this is cons');
+    const array2 = this.array;
+    this.array.push(6);
+    array2.push(6);
+    console.log(array2);
+    console.log(this.array);
+  }
 
-  ngOnInit(){
+  ngOnInit() {
     this.loginForm = this.fb.group({
       username: ['', [Validators.required]],
       password: ['', [Validators.required]],
-    })
+    });
 
     this.password = 'password';
+    console.log('this is oninit');
   }
 
-  onSubmit(){
-    if(this.loginForm.valid){
+  onSubmit() {
+    if (this.loginForm.valid) {
       this.authServices.login(this.loginForm.value);
-    }else{
+    } else {
       console.log('false');
     }
   }
-
-
 
   showHide() {
     if (this.password === 'password') {
